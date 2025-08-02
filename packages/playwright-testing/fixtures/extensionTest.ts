054754c85de3613b598756be9d24cb60ae403136
Your Name <you@example.com>
2025-08-02 15:14:07 -0400

    Added playwright testing for extension

---
+++ packages/playwright-testing/fixtures/extensionTest.ts
@@ -0,0 +1,36 @@
+/* eslint-disable import/named */
+import { type BrowserContext, test as base, chromium } from '@playwright/test';
+import path from 'path';
+
+export const test = base.extend<{
+  context: BrowserContext;
+  extensionId: string;
+}>({
+  // eslint-disable-next-line no-empty-pattern
+  context: async ({}, use) => {
+    const pathToExtension = path.join(__dirname, '../extension');
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [
+        process.env.CI ? `--headless=new` : '',
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    // for manifest v3:
+    let [background] = context.serviceWorkers();
+    if (!background) {
+      background = await context.waitForEvent('serviceworker');
+    }
+
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+
+export const expect = test.expect;
---
+++ packages/playwright-testing/readme.md
@@ -0,0 +1,11 @@
+# Playwright Testing Setup
+
+## Commands
+
+```sh
+pnpm playwright install chromium
+pnpm playwright test
+```
+
+- `pnpm playwright install chromium`: Installs the Chromium browser for Playwright testing.
+- `pnpm playwright test`: Runs all Playwright tests in the project.
